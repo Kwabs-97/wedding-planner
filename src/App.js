6f270ed9a25d4b4b865401efdb59a3b4eedfab88
Kwabs-97 <khobewayne@gmail.com>
2023-09-25 17:01:42 +0000

    app module css fixed gap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 import Services from "./Components/Services";
 
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Navbar />
       <Hero />
       <Services />
