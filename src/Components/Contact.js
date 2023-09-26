6d08238dc1d80ff23b02b9181743d2ff17878c31
Kwabs-97 <khobewayne@gmail.com>
2023-09-26 08:04:17 +0000

    contact layout done

---
+++ src/Components/Contact.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+import Styles from "../Styles/Contact.module.css";
+import Button from "./Features/Button";
+
+function Contact() {
+  return (
+    <div className={Styles.contact}>
+      <div className={Styles.container}>
+        <header></header>
+        <p></p>
+        <Button></Button>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
