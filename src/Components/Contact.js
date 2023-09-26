0f8e3ce7b5693b070c868464389cb0fbb644098d
Kwabs-97 <khobewayne@gmail.com>
2023-09-26 08:05:12 +0000

    git jsx filled

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,9 +8,12 @@ function Contact() {
   return (
     <div className={Styles.contact}>
       <div className={Styles.container}>
-        <header></header>
-        <p></p>
-        <Button></Button>
+        <header>Connect With Us and Begin Your Journey to a Perfect Wedding</header>
+        <p>
+          Our team is here to guide you every step of the way, ensuring that your love story is
+          beautifully woven into every detail.
+        </p>
+        <Button>Start planning</Button>
       </div>
     </div>
   );
