3351392b505affc3a04e59c0fcb65c242d04893e
Kwabs-97 <khobewayne@gmail.com>
2023-09-27 16:47:52 +0000

    react scroll to footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,6 +5,8 @@ import styles from "../Styles/Footer.module.css";
 import { Link, animateScroll as scroll } from "react-scroll";
 import Button from "./Features/Button";
 
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+
 const footerLinks = [
   {
     id: 1,
@@ -24,6 +26,10 @@ const footerLinks = [
   },
 ];
 function Footer() {
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  }
   const [hasValue, setHasValue] = useState(false);
 
   const handleOnChange = (e) => {
@@ -38,30 +44,36 @@ function Footer() {
 
   return (
     <footer>
-      <div className={styles.footerContainer}>
-        <div className={styles.footerLinks}>
-          <ul className={styles.links}>
-            {" "}
-            <li>
-              {" "}
-              {footerLinks.map((footerLink) => (
-                <Link to={footerLink.link} key={footerLink.id}>
-                  {footerLink.link}
-                </Link>
-              ))}
-            </li>
-          </ul>
+      <div className={styles.container}>
+        <div className={styles.footerContainer}>
+          <div className={styles.footerLinks}>
+            <ul className={styles.links}>
+              <li>
+                {footerLinks.map((footerLink) => (
+                  <Link to={footerLink.link} key={footerLink.id} smooth={true} duration={500}>
+                    {footerLink.link}
+                  </Link>
+                ))}
+              </li>
+            </ul>
+          </div>
+          <div className={styles.inputContainer}>
+            <input type="email" id="email" onChange={handleOnChange} className={inputClass} />
+            <label htmlFor="email" className={styles.label}>
+              Email address
+            </label>
+            <Button>Subscribe</Button>
+          </div>
         </div>
-        <div className={styles.inputContainer}>
-          <input type="email" id="email" onChange={handleOnChange} className={inputClass} />
-          <label htmlFor="email" className={styles.label}>
-            Email address
-          </label>
-          <Button>Subscribe</Button>
+        <div className={styles.line}></div>
+        <div className={styles.socialsContainer}>
+          <FaInstagram style={{ width: "1.25rem", height: "1.25rem" }} />
+          <FaFacebook style={{ width: "1.25rem", height: "1.25rem" }} />
+          <FaLinkedin style={{ width: "1.25rem", height: "1.25rem" }} />
+          <FaTwitter style={{ width: "1.25rem", height: "1.25rem" }} />
+          <FaYoutube style={{ width: "1.25rem", height: "1.25rem" }} />
         </div>
       </div>
-      <div className={styles.line}></div>
-      <div className={styles.socialsContainer}></div>
     </footer>
   );
 }
